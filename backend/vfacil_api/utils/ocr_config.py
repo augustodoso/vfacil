02485d20b709cc8eda8620c0b59198d47feb4ff7
augustodoso <augustocezaruniverso@gmail.com>
2025-12-05 00:40:02 -0300

    fix: remover arquivo vfacil corrompido

--- backend/vfacil_api/utils/ocr_config.py
+++ backend/vfacil_api/utils/ocr_config.py
@@ -1,7 +1,13 @@
+import os
 import pytesseract
 
-# Caminho exato do Tesseract no Windows
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Configura o caminho do Tesseract de forma diferente para Windows e Linux
+if os.name == "nt":  # Windows
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+else:  # Linux (Render)
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+
 
 try:
     _ = pytesseract.get_tesseract_version()
